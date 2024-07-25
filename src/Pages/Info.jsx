d647cb6eca9d54a1ba69fddc31c48b9643d9ec58
ScaledAdder <avandiayovan@gmail.com>
2024-07-25 12:52:07 +0700

    update pages info

---
+++ src/Pages/Info.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Body from './InfoPartition/InfoBody'
+import Intro from './InfoPartition/InfoIntro'
+
+function Info() {
+  return (
+    <>
+      <Intro />
+      <Body />
+    </>
+  )
+}
+
+export default Info
