60b5d408a5ccc6c1fc0287e5765633c97e8b133c
ScaledAdder <avandiayovan@gmail.com>
2024-07-25 12:58:03 +0700

    update body info

--- src/Pages/InfoPartition/InfoBody.jsx
+++ src/Pages/InfoPartition/InfoBody.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import logoDesa from "../../assets/images/logoDesaJepitu.png";
-import calendar from "../../assets/images/calendar.svg"
+import calendar from "../../assets/images/calendar.svg";
 
 function Body() {
-
   return (
     <>
       <div>
-        <div className='text-black m-5 p-2 border rounded-md bg-white bg-opacity-80'>
-          <h1 className='text-2xl font-bold'>Pentingnya Pendidikan,</h1>
-          <h1 className='text-2xl font-bold'>Demi Sumber Daya Dusun</h1>
+        <div className="text-black m-5 p-2 border rounded-md bg-white bg-opacity-80">
+          <h1 className="text-2xl font-bold">Ketik Judul Informasi & Berita</h1>
           <div className="py-2 md:w-15 lg:w-20 grid grid-cols-2 gap-2 text-xs">
             <div className="flex items-center col-span-1 ">
               <img
@@ -18,23 +16,31 @@ function Body() {
                 alt="Logo"
                 className="object-contain w-5 mr-2"
               />
-              <p className=''>Karanglor</p>
+              <p className="">Karanglor</p>
             </div>
-            
+
             <div className="flex items-center col-span-1">
-              <img
-                src={calendar}
-                alt="Calendar"
-                className="w-5 mr-2"
-              />
+              <img src={calendar} alt="Calendar" className="w-5 mr-2" />
               <p>Senin, 22 Juli 2024</p>
             </div>
           </div>
-          <p className='text-sm font-light mt-4'>
-          Halo para pembaca sekalian! Pada kesempatan kali ini, Kelompok 13 KKN 85 UAJY ingin mengajak Kita semua untuk menjelajahi kisah di dalam Dusun Karanglor tercinta kita. Bersama-sama, kita akan membahas mengenai Pendidikan Dusun yang dimana seperti kita tahu hal yang mendasar merupakan hal yang paling berdampak besar. Pada pembahasan kali ini yakni Pendidikan, itu sendiri yang merupakan tumpuan dari sebuah Bangsa, terkhususnya di Dusun Karanglor tercinta.
+          <p className="text-sm font-light mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <p className='text-sm font-light mt-2'>
-          Pendidikan di Dusun Karanglor tidak hanya bertujuan mencetak lulusan yang berprestasi secara akademis, tetapi juga membentuk individu yang mampu berkontribusi positif bagi masyarakat. Dengan pendidikan yang baik, sumber daya manusia di dusun dapat meningkat kualitasnya, yang pada akhirnya akan berkontribusi pada kemajuan dusun itu sendiri.
+          <p className="text-sm font-light mt-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
       </div>
