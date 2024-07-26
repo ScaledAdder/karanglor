badcda84cdce365b8c24eff70cf680546a5946d7
ScaledAdder <93301767+ScaledAdder@users.noreply.github.com>
2024-07-26 21:24:45 +0700

    ubah deskripsi rasulan

--- src/Pages/InfoPartition/InfoBody.jsx
+++ src/Pages/InfoPartition/InfoBody.jsx
@@ -29,13 +29,7 @@ function Body() {
           </p>
           
           <p className="text-sm font-light mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            srdtyuhgcxcvbmnmbvcbnmvbcxghewredgvbcxcgdrswer6trysfdgjvbccbvsdaerytdfgbv v456eracxdw56reshgfcv
           </p>
 
            <p className="text-sm font-light mt-4">
